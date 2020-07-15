7f7a3bcbd45c38a135b62ae55d6c8728807d9a56
Bharat Singh <bksingh@g.cofc.edu>
2020-07-15 09:07:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SignalR2-.Net4.7.2
---
+++ RealTimeChat/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void LogError(ErrorModel errorModel)
+        {
+            Clients.All.logErrorToPage(errorModel);
+        }
+    }
+    public class ErrorModel
+    {
+        public string Message { get; set; }
+        public string Source { get; set; }
+        public int LineNo { get; set; }
+        public int ColNo { get; set; }
+        public string Error { get; set; }
+    }
+}
